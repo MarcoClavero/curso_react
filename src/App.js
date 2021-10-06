21febac9f03e96474a4a5495846669ff5a101d2e
MarcoClavero <marco.clavero@mail.udp.cl>
2021-10-06 05:09:14 -0300

    update 4

--- src/App.js
+++ src/App.js
@@ -1,30 +1,3 @@
-<<<<<<< HEAD
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
-
-export default App;
-=======
 import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
@@ -62,7 +35,7 @@ class App extends Component {
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>Propagando el state de nuestros componentes</p>
-        <Contador contadorInicial={10} /> 
+        <Contador contadorInicial={50} /> 
 
         </div>
        
@@ -73,4 +46,3 @@ class App extends Component {
 
 
 export default App;
->>>>>>> 83538e9060a55f10dbe853e2bfe69995812a9579
